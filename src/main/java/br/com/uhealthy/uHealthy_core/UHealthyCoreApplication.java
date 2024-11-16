470822bde34589ce43f60092697b78814944311f
Rogerio Gomes <rogerio.gomes@globant.com.br>
2024-11-16 01:57:06 -0300

    Initializer

---
+++ src/main/java/br/com/uhealthy/uHealthy_core/UHealthyCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.uhealthy.uHealthy_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UHealthyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UHealthyCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uhealthy/uHealthy_core/UHealthyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uhealthy.uHealthy_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UHealthyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
